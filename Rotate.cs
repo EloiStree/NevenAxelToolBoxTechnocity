686954eae5eb5290074e85f127dd620d8b51bd8e
Neven Axel <neven.axel@gmail.com>
2020-06-15 13:23:12 +0200

    adding assembly runtime and editor

---
+++ Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public int m_myAge;
+
+    public int getAgeOfUser() { return m_myAge; }
+    public void setAgeOfUser(int ageValue) { m_myAge = ageValue; }
+
+    private void Update()
+    {
+        
+    }
+
+    private Rotate m_target;
+}
